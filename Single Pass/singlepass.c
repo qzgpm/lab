369fd2ac1d63def7c3b1aaf3851cb317b35ddc47
Delvin <dlvn3jai@gmail.com>
2025-10-17 15:32:50 +0530

    text length logic added to single pass

--- Single Pass/singlepass.c
+++ Single Pass/singlepass.c
@@ -5,38 +5,49 @@
 char *my_itoa(int num, char *str)
 {
     if (str == NULL)
-    {
         return NULL;
-    }
     sprintf(str, "%d", num);
     return str;
 }
 
-void main()
+int main()
 {
     FILE *f1, *f2, *f3, *f4, *f5;
     int lc, sa, i = 0, j = 0, m[10], pgmlen, len, k, len1, l = 0;
+    int textlen = 0;
     char name[10], opnd[10], la[10], mne[10], s1[10], mne1[10], opnd1[10];
     char lcs[10], ms[10];
     char sym[10], symaddr[10], obj1[10], obj2[10], s2[10], q[10], s3[10];
+
     f1 = fopen("input.txt", "r");
     f2 = fopen("optab.txt", "r");
     f3 = fopen("symtab.txt", "w+");
     f4 = fopen("symtab1.txt", "w+");
     f5 = fopen("output.txt", "w+");
+
+    if (!f1 || !f2 || !f3 || !f4 || !f5)
+    {
+        printf("Error opening files.\n");
+        return 1;
+    }
+
     fscanf(f1, "%s%s%s", la, mne, opnd);
+
     if (strcmp(mne, "START") == 0)
     {
         sa = atoi(opnd);
         strcpy(name, la);
         lc = sa;
     }
+
     strcpy(s1, "*");
     fscanf(f1, "%s%s%s", la, mne, opnd);
+
     while (strcmp(mne, "END") != 0)
     {
         if (strcmp(la, "-") == 0)
         {
+            fseek(f2, 0, SEEK_SET);
             fscanf(f2, "%s%s", mne1, opnd1);
             while (!feof(f2))
             {
@@ -45,18 +56,17 @@ void main()
                     m[i] = lc + 1;
                     fprintf(f3, "%s\t%s\n", opnd, s1);
                     fprintf(f5, "%s\t0000\n", opnd1);
-                    lc = lc + 3;
-                    i = i + 1;
+                    lc += 3;
+                    textlen += 3;
+                    i++;
                     break;
                 }
-                else
-                    fscanf(f2, "%s%s", mne1, opnd1);
+                fscanf(f2, "%s%s", mne1, opnd1);
             }
         }
-
         else
         {
-            fseek(f3, SEEK_SET, 0);
+            fseek(f3, 0, SEEK_SET);
             fscanf(f3, "%s%s", sym, symaddr);
             while (!feof(f3))
             {
@@ -65,48 +75,51 @@ void main()
                     my_itoa(lc, lcs);
                     fprintf(f4, "%s\t%s\n", la, lcs);
                     my_itoa(m[j], ms);
-                    j = j + 1;
+                    j++;
                     fprintf(f5, "%s\t%s\n", ms, lcs);
-                    i = i + 1;
+                    i++;
                     break;
                 }
-                else
-                    fscanf(f3, "%s%s", sym, symaddr);
-            } 
+                fscanf(f3, "%s%s", sym, symaddr);
+            }
+
             if (strcmp(mne, "RESW") == 0)
-                lc = lc + 3 * atoi(opnd);
+                lc += 3 * atoi(opnd);
+            else if (strcmp(mne, "RESB") == 0)
+                lc += atoi(opnd);
             else if (strcmp(mne, "BYTE") == 0)
             {
                 strcpy(s2, "-");
                 len = strlen(opnd);
-                lc = lc + len - 2;
-                for (k = 2; k < len; k++)
+                lc += len - 3;
+                textlen += len - 3;
+                for (k = 2; k < len - 1; k++)
                 {
-                    q[l] = opnd[k];
-                    l = l + 1;
+                    q[l++] = opnd[k];
                 }
+                q[l] = '\0';
                 fprintf(f5, "%s\t%s\n", q, s2);
-                break;
             }
-            else if (strcmp(mne, "RESB") == 0)
-                lc = lc + atoi(opnd);
             else if (strcmp(mne, "WORD") == 0)
             {
                 strcpy(s3, "#");
-                lc = lc + 3;
+                lc += 3;
+                textlen += 3;
                 fprintf(f5, "%s\t%s\n", opnd, s3);
-                break;
             }
         }
 
-        fseek(f2, SEEK_SET, 0);
+        fseek(f2, 0, SEEK_SET);
         fscanf(f1, "%s%s%s", la, mne, opnd);
     }
-    fseek(f5, SEEK_SET, 0);
+
+    fseek(f5, 0, SEEK_SET);
     pgmlen = lc - sa;
+
     printf("H^%s^%d^0%x\n", name, sa, pgmlen);
     printf("T^");
-    printf("00%d^0%x", sa, pgmlen);
+    printf("00%d^0%x", sa, textlen);
+
     fscanf(f5, "%s%s", obj1, obj2);
     while (!feof(f5))
     {
@@ -117,16 +130,16 @@ void main()
             printf("^");
             len1 = strlen(obj1);
             for (k = 0; k < len1; k++)
-                printf("%d", obj1[k]);
+                printf("%X", obj1[k]);
         }
         else if (strcmp(obj2, "#") == 0)
         {
-            printf("^");
-            printf("%s", obj1);
+            printf("^%s", obj1);
         }
         fscanf(f5, "%s%s", obj1, obj2);
     }
-    fseek(f5, SEEK_SET, 0);
+
+    fseek(f5, 0, SEEK_SET);
     fscanf(f5, "%s%s", obj1, obj2);
     while (!feof(f5))
     {
@@ -143,5 +156,14 @@ void main()
         }
         fscanf(f5, "%s%s", obj1, obj2);
     }
-    printf("\nE^00%d", sa);
+
+    printf("\nE^00%d\n", sa);
+
+    fclose(f1);
+    fclose(f2);
+    fclose(f3);
+    fclose(f4);
+    fclose(f5);
+
+    return 0;
 }
